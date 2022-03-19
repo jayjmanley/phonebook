310656cdf99a91345d2808496bc0c5d5500ca0e7
Jay Manley <jayjmanley@gmail.com>
2022-03-19 17:40:47 -0600

    Added error message for when there is duplicate user

--- src/index.js
+++ src/index.js
@@ -5,20 +5,13 @@ import reportWebVitals from './reportWebVitals';
 
 const Search = ({list, filter}) =>
 {
-    const [name, setName] = useState(void 0);
-
-    function search(event){
-        event.preventDefault();
-        filter(event.target.value.toLowerCase());
-    }
-
     return (
         <div>
             <h1>Search</h1>
-            <form onSubmit={search}>
-                <input onChange={(event) => search(event)}/>
-                <input onChange={(event) => search(event)}/>
-                <button type="submit">Search</button>
+            <form>
+                First Name: <input onChange={event => filter(event.target.value)}/>
+                Last Name: <input onChange={event => filter(event.target.value)}/>
+                Phone Number: <input onChange={event => filter(event.target.value)}/>
             </form>
         </div>
     )
@@ -31,39 +24,30 @@ const App = () =>
     ]);
 
 
-    const [ formInfo, setNewFormInfo ] = useState({firstname: '', lastname: '', phone: ''});
+    const [{ firstname, lastname, phone, errorMessage = '' }, setNewFormInfo] = useState({ firstname: '', lastname: '', phone: '' });
 
     function addPerson(event){
         event.preventDefault();
-        setList(list.concat({firstname:formInfo.firstname, lastname:formInfo.lastname, phone:formInfo.phone, show:true}))
-        // let canSearch = true;
 
-        // list.forEach(person => { // checks if the person's name is already added
-        //     if(person.firstname == formInfo.firstname)
-        //     canSearch = false;       
-        // });
+        const duplicate = list.some(list => (list.firstname === firstname) && (list.lastname === lastname) && (list.phone === phone));
 
-        // if(canSearch)
-        //     setList(list.concat({firstname:formInfo.firstname, phone:formInfo.phone, show:true}));
-        // else
-        //     alert(`${formInfo.firstname} is already added to the list!`);
-     }
-
-    function filter(nameToFilter){
-        if(nameToFilter === ''){ // if the search form is empty, then show everyone
-            setList((prev) => prev.map(person => ({...person, show:true})));
-        }
-        else{
-            const filtered = [];
-            list.forEach(person => {
-                if(!person.firstname.toLowerCase().includes(nameToFilter))
-                    filtered.push({...person, show:false});
-                else
-                    filtered.push({...person});
-            });
-            setList(filtered);
-        }
+        if (!duplicate)
+            setList(list.concat({firstname, lastname, phone, show: true }));
+        else
+            setNewFormInfo({firstname, lastname, phone, errorMessage: `${firstname} ${lastname} with number ${phone} is already added.` });
     }
+
+
+  function filter(filteredValue) {
+      const lower = filteredValue.toLowerCase();
+      setList(list.map(list =>
+          ({
+              ...list,
+              show: list.firstname.toLowerCase().includes(lower),
+          })
+      ));
+  }
+
 
     return (
         <div>
@@ -72,41 +56,31 @@ const App = () =>
             <h2>Add Contact</h2>
             <form onSubmit={addPerson}>
                 <div>
-                    First Name: <input onChange={(event) => setNewFormInfo({firstname: event.target.value, lastname: formInfo.lastname, phone: formInfo.phone})}/>
+                    First Name: <input onChange={(event) => setNewFormInfo({firstname: event.target.value, lastname, phone})}/>
                     <br/>
-                    Last Name: <input onChange={(event) => setNewFormInfo({firstname: formInfo.firstname, lastname: event.target.value, phone: formInfo.phone})}/>
+                    Last Name: <input onChange={(event) => setNewFormInfo({firstname, lastname: event.target.value, phone})}/>
                     <br/>
-                    Number: <input onChange={(event) => setNewFormInfo({firstname: formInfo.firstname, lastname: formInfo.lastname, phone: event.target.value})}/>
+                    Number: <input onChange={(event) => setNewFormInfo({firstname, lastname, phone: event.target.value})}/>
                 </div>
                 <div>
                     <button type="submit">Add Contact</button>
                 </div>
+                {(errorMessage && <div className="error">{errorMessage}</div>)}
             </form>
             <h2>List of Contacts</h2>
             <List list={list} />
         </div>
     )
 }
-const List = ({list}) => 
-{
-    function renderList(){
-        const elements = [];
-
-        list.forEach(person => {
-            if(person.show){
-                elements.push(<li><b>Name:</b> {person.firstname} {person.lastname} <b>Number:</b> {person.phone}</li>);
-            }
-        });
-
-        return elements;
-    }
-
-    return(
-        <ul>
-            {renderList()}
-        </ul>
-    )
-}
+const List = ({list}) => (
+      <ul>
+        {(
+          list
+            .filter(list => list.show)
+            .map(list => <li><b>Name:</b> {list.firstname} {list.lastname} <b>Number:</b> {list.phone}</li>)
+        )}
+      </ul>
+);
 
 
 
