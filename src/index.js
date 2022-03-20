277bdd4b46b79e591d9133b93b770af3cee2e24e
Jay Manley <jayjmanley@gmail.com>
2022-03-19 20:34:48 -0600

    changed it back to left align

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import './App.css';
 
 
 ReactDOM.render(<App />, document.getElementById("root"))
